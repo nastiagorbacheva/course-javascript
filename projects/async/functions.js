ead1121def16a8390203112221a4eabba37608a5
Anastasiya Gorbacheva <anastasya.bortnik@yandex.ru>
2021-05-07 23:58:28 +0700

    Сделано ДЗ

--- projects/async/functions.js
+++ projects/async/functions.js
@@ -8,7 +8,13 @@
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function delayPromise(seconds) {}
+function delayPromise(seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, seconds * 1000);
+  });
+}
 
 /*
  Задание 2:
@@ -23,6 +29,30 @@ function delayPromise(seconds) {}
  Пример:
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
-function loadAndSortTowns() {}
+function loadAndSortTowns() {
+  function nameSort(a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+  }
+  const promise = new Promise(function (resolve, reject) {
+    fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        reject('не удалось загрузить страницу');
+      })
+      .then((towns) => {
+        towns.sort(nameSort);
+
+        return resolve(towns);
+      });
+  });
+  return promise;
+}
 
 export { delayPromise, loadAndSortTowns };
